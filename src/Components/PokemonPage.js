7d8eca6eb6ad0e5ef95976f88a3d3d7aeb71c5d2
“Levar <levar.pitts@gmail.com>
2023-02-18 01:30:41 -0500

    Stat card
    finished stat card with gauge

--- src/Components/PokemonPage.js
+++ src/Components/PokemonPage.js
@@ -1,10 +1,14 @@
 import React, { useState } from 'react'
-import  Modal  from 'react-bootstrap/Modal'
+import Modal from 'react-bootstrap/Modal'
 import Button from 'react-bootstrap/Button';
 import '../styles/style.css'
+import { ModalFooter } from 'react-bootstrap';
 
 export default function PokemonPage({data, show, handleClose}) {
-  
+
+  if(!show){
+    return null
+  }
 
 const getColor=(item)=> {
   switch(data.types[0].type.name) {
@@ -57,38 +61,53 @@ const getColor=(item)=> {
             <>
             <Modal 
               show={show} 
-              onHide={()=>{}} 
-              backdrop='static'
+              onHide={handleClose} 
+              backdrop='true'
               keyboard={false} 
+              animation={true}
             >
+              <Modal.Header className=''>
+              <div className='pokemon-name' style={{color: `${getColor(data)}`}}>{data.name}</div>
+              </Modal.Header>
               <div className='whole-modal'>
               <div className='modal_pop_out'  style={{backgroundColor: `${getColor(data)}`}}>
-              <div onClick={handleClose} className='back-button-container'>x</div>
-                <div className='pokemon-name'>{data.name}</div>
-                <div className='pokemon-number' style={{color: `${getColor(data)}`}}>{data.id}</div>
                 <div className='type-container'>
                 {data.types.map(type=>{
                   return(
                     <div className='type_button_card' style={{color: `${getColor(data)}`}}>{type.type.name}</div>
                   )
                 } )}
+                <div className='pokemon-number' style={{color: `${getColor(data)}`}}>{data.id}</div>
                </div>
                 <div className='stat-card'>   
                 <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/
 other/dream-world/${data.id}.svg`} alt="" className='main-pic'/>
+<div className='stat-div'>
                    {
                         data.stats.map(poke=>{
                             return(
                                 <>
-                                    <h3>{poke.stat.name}:{poke.base_stat}</h3>
+                                    <div className='pokemon-stats'>
+                                      <div className='stat-name'  style={{backgroundColor: `${getColor(data)}`}}>
+                                      {poke.stat.name}
+                                      </div>
+                                      <div className='guage-background'  style={{backgroundColor: `${getColor(data)}`}}>
+                                        <div className='guage' style={{width: `${poke.base_stat * 1.1}px`}}/>
+                                      </div>
+                                      <div className='stat-number' style={{backgroundColor: `${getColor(data)}`}}>
+                                      {poke.base_stat}
+                                      </div>
+                                    </div>
                                 </>
                             )
                         })
                     }
-               
+               </div>
                 </div>
                 </div>
                 </div>
+                
+                
               </Modal>
             </>
         )
